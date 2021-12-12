eb3711ca6eaa9d6daf9e29c38ce7aafbfd79f16c
vntutb05 <52446973+vntutb05@users.noreply.github.com>
2021-12-12 13:30:09 +0700

    Update database.js

--- BackEnd/core/database.js
+++ BackEnd/core/database.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql")
 
-const conn = mysql.createConnection('mysql://root:EMuSYyBOhRN2JLBGSq88@containers-us-west-6.railway.app:6558/railway')
+const conn = mysql.createConnection('mysql://root:05052000@containers-us-west-6.railway.app:6558/railway')
 conn.connect(function(err) {
     if (err) throw err;
     console.log("Connected!!!")
