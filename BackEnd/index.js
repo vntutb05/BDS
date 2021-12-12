55c0bcca4cdd985351520d86ffd3477f3d176b96
Huy Loc <74889392+HuyLoc41@users.noreply.github.com>
2021-12-12 11:44:34 +0700

    cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended :false}))
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 app.use('/static/image', express.static(__dirname + '/upload'));
 // app.use(userUpload)
 //PORT
